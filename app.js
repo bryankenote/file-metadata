27bbec550bf0646891ec32c1cf84fe1db828e72b
Bryan <brykenote@gmail.com>
2017-03-20 02:54:19 +0000

    upload files and display file size

--- app.js
+++ app.js
@@ -29,7 +29,8 @@ app.use(require('node-sass-middleware')({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(multer({ dest: './uploads/'}));
+//app.use(multer({ dest: './uploads/'}));
+app.use(multer({dest:'./uploads/'}).single('file'));
 
 app.use('/', index);
 app.use('/users', users);
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/upload', function(req, res) {
+    let metadata = {
+        "size": req.file.size
+    }
+    res.send(JSON.stringify(metadata));
+});
+
 module.exports = router;
